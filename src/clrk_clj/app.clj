4c61df0a3558ca2b918839aecd7c0c610ea6a6b4
zax <zd@zdsmith.com>
2016-01-24 00:19:04 -0500

    Import anything into db when it's requested on API

--- src/clrk_clj/app.clj
+++ src/clrk_clj/app.clj
@@ -1,5 +1,6 @@
 (ns clrk-clj.app
   (:require [clrk-clj.omdb :as omdb]
+            [clrk-clj.core :as core]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :as route]
             [ring.util.response :refer [response]]
@@ -8,8 +9,15 @@
             [ring.adapter.jetty :require [run-jetty]])
   (:gen-class))
 
+(defn get-and-respond
+  "Look up a movie and then respond with the OMDB server's response"
+  [title]
+  (core/get-movie title)
+  (let [search-result (first (omdb/find-movie title))]
+    (omdb/get-movie search-result)))
+
 (defroutes movie-routes
-  (GET "/movies/titles/:title" [title] (response (first (omdb/find-movie title))))
+  (GET "/movies/titles/:title" [title] (response (get-and-respond title)))
   (GET "/movies/:id" [id] (response (omdb/get-movie id)))
   (route/not-found (response {:error "Not Found"})))
 
